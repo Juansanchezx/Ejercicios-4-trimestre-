d63ad527d7520bd371afe9ad746f71c5cc6cfa8e
Juan Sanchez <jjtoti@gmail.com>
2024-05-03 18:09:02 -0500

    trimestre

--- Colores.js
+++ Colores.js
@@ -1,64 +1,78 @@
 console.log("Colores");
 
-let botonAmarillo = document.querySelector("#Amarillo");
-let botonRojo= document.querySelector("#Rojo");
-let botonAzul= document.querySelector("#Azul");
+let botonAmarillo = document.querySelector("#amarillo");
+let botonRojo = document.querySelector('#Rojo');
+let botonAzul = document.querySelector('#Azul');
+let botonGris = document.querySelector('#gris');
+let botonSinColor = document.querySelector('#sinColor');
 
-
-// let body = document.getElementsByTagName("body");
-
-botonAmarillo.addEventListener("click", 
-    function mensaje(){
-        console.log("Buscando el color Amarillo");
-        document.body.style.backgroundColor = 'yellow';
-    }
-)
-botonRojo.addEventListener("click", 
-    function mensaje(){
-        console.log("Buscando el color rojo");
-        document.body.style.backgroundColor = 'Red';
-    }
-)
-botonAzul.addEventListener("click", 
-    function mensaje(){
-        console.log("Buscando el color azul");
-        document.body.style.backgroundColor = 'blue';
-    }
-)
+let body = document.querySelector("body")
+let cuadrito = document.querySelector("#cuadrito")
+let cuadrobotonAmarillo = document.querySelector("#cuadroAmarillo");
+let cuadrobotonRojo = document.querySelector('#cuadroRojo');
+let cuadorbotonGris = document.querySelector('#cuadroGris');
+let cuadrobotonSinColor = document.querySelector('#cuadroSinColor');
 
 
 
-let body = document.querySelector("body");
-let cuadro = document.querySelector("cuadro");
-let cuadrobotonAmarillo= document.querySelector("#Amarillo");
-let cuadrobotonRojo= document.querySelector("#Rojo");
-let cuadrobotonAzul= document.querySelector("#Azul");
-let cuadrobotonGris= document.querySelector("#Gris");
-let cuadrobotonSincolor= document.querySelector("#Sincolor");
-
-cuadritobotonAmarillo.addEventListener("click", 
+cuadrobotonAmarillo.addEventListener("click", 
     function mensaje(){
         console.log("Buscando el color amarillo");
         cuadrito.style.backgroundColor = 'yellow';
     }
 )
-cuadritobotonRojo.addEventListener("click", 
+cuadrobotonRojo.addEventListener("click", 
     function mensaje(){
         console.log("Buscando el color rojo");
         cuadrito.style.backgroundColor = 'red';
     }
 )
-cuadritobotonAzul.addEventListener("click", 
-    function mensaje(){
-        console.log("Buscando el color Azul");
-        cuadrito.style.backgroundColor = 'blue';
-    }
-)
-cuadritobotonGris.addEventListener("click", 
+
+cuadorbotonGris.addEventListener("click", 
     function mensaje(){
         console.log("Buscando el color gris");
         cuadrito.style.backgroundColor = 'gray';
     }
 )
+cuadrobotonSinColor.addEventListener("click", 
+    function mensaje(){
+        console.log("Buscando el color sin color");
+        cuadrito.style.backgroundColor = 'transparent';
+    }
+)
 
-console.log(cuadrito)
+
+botonAmarillo.addEventListener("click", 
+    function mensaje(){
+        console.log("Buscando el color amarillo");
+        document.body.style.backgroundColor = 'yellow';
+    }
+)
+botonRojo.addEventListener("click", 
+    function mensaje(){
+        console.log("Buscando el color Rojo");
+        document.body.style.backgroundColor = 'red';
+    }
+)
+botonAzul.addEventListener("click", 
+    function mensaje(){
+        console.log("Buscando el color Azul");
+        document.body.style.backgroundColor = 'blue';
+    }
+)
+botonGris.addEventListener("click", 
+    function mensaje(){
+        console.log("Buscando el color Gris");
+        document.body.style.backgroundColor = 'gray';
+    }
+)
+botonSinColor.addEventListener("click", 
+    function mensaje(){
+        console.log("Sin Color");
+        document.body.style.backgroundColor = 'transparent';
+    }
+)
+
+
+
+console.log(cuadrito);
